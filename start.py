b5ae7d49b89f1912745a8355e935d07aa39f3e07
Fraser Dale <fraserdale2@gmail.com>
2019-04-09 11:47:32 +0100

    comments added, imporved routing

--- start.py
+++ start.py
@@ -1,40 +1,63 @@
 import pymongo
 from bson import ObjectId
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect
 
+#setup flask app
 app = Flask(__name__)
+
+# Mongo client connection
 MONGODB_URI = "mongodb://fraser:Secure_1@ds135036.mlab.com:35036/address-book"
 mongo_client = pymongo.MongoClient(MONGODB_URI, connectTimeoutMS=30000)
+# Select address-book database and initialise the organisations and employees collections
 db = mongo_client.get_database("address-book")
 organisations = db.organisations
 employees = db.employees
 
 
-@app.route("/",methods=['GET','POST'])
+# home index route, returns all the organisations, names and contact details.
+@app.route("/",methods=['GET'])
 def home():
-	if request.method == 'POST':
-		request_information = request.form
-		organisations.insert({
-			"name": request_information['name'],
-			"contact": request_information['contact']
-		})
 	organisationsDump = organisations.find({})
 	return render_template('index.html',organisations=organisationsDump)
 
-@app.route('/updateOrganisation',methods=['GET','POST'])
+# create new organisation, inserts name and contact details
+# from post request into organisation collection then redirects
+@app.route('/organisation',methods=['POST'])
+def newOrganisation():
+	request_information = request.form
+	organisations.insert({
+		"name": request_information['name'],
+		"contact": request_information['contact']
+	})
+	return redirect("/", code=302)
+
+# recieves get request, based on query string ID parameter returns
+# different organisation information
+@app.route('/organisation',methods=['GET'])
+def getOrganisation():
+	# error handling if no id is provided
+	if request.args.get('id') == None:
+		return redirect("/", code=302)
+	organisation_id = request.args.get('id')
+	organisation = organisations.find_one({"_id": ObjectId(organisation_id)})
+	employeesDump = employees.find({"organisationID": organisation_id})
+	return render_template('organisation.html', organisation=organisation, employees=employeesDump)
+
+# updates the information about the organisation
+# based on the parameters given in post form data
+@app.route('/updateOrganisation',methods=['POST'])
 def updateOrganisation():
-	if request.method == 'POST':
-		request_information = request.form
-		organisations.update_one({'_id': ObjectId(request_information['id'])}, {
-				'$set': {"name":request_information['name'],"contact":request_information['contact']}
-			}, upsert=False)
-		organisation_id = request_information['id']
-	if request.method == 'GET':
-		organisation_id = request.args.get('id')
+	request_information = request.form
+	organisations.update_one({'_id': ObjectId(request_information['id'])}, {
+			'$set': {"name":request_information['name'],"contact":request_information['contact']}
+		}, upsert=False)
+	organisation_id = request_information['id']
 	organisation = organisations.find_one({"_id": ObjectId(organisation_id)})
 	employeesDump = employees.find({"organisationID":organisation_id})
 	return render_template('organisation.html', organisation=organisation,employees=employeesDump)
 
+# deletes the organisation specified
+# deletes all the employees that belong to the company
 @app.route('/deleteOrganisation',methods=['POST'])
 def deleteOrganisation():
 	request_information = request.form
@@ -43,34 +66,45 @@ def deleteOrganisation():
 	organisationsDump = organisations.find({})
 	return render_template('index.html',organisations=organisationsDump)
 
-
-@app.route('/addEmployee',methods=['GET','POST'])
-def addEmployee():
-	if request.method == 'POST':
-		request_information = request.form
-		employees.insert({"name": request_information['name'], "contact": request_information['contact'],
- 						  "organisationID": request_information['id']})
-		organisation_id = request_information['id']
-	if request.method == 'GET':
-		organisation_id = request.args.get('id')
-	organisation = organisations.find_one({"_id": ObjectId(organisation_id)})
-	employeesDump = employees.find({"organisationID":organisation_id})
-	return render_template('organisation.html', organisation=organisation,employees=employeesDump)
-
-@app.route('/updateEmployee',methods=['GET','POST'])
-def updateEmployee():
-	if request.method == 'POST':
-		request_information = request.form
-		employees.update_one({'_id': ObjectId(request_information['id'])}, {
-			'$set': {"name": request_information['name'], "contact": request_information['contact']}
-		}, upsert=False)
-		employee_id = request_information['id']
-	if request.method == 'GET':
-		employee_id = request.args.get('id')
+# employee page displays name and contact information about
+# the employee specified in querystring
+@app.route('/employee',methods=['GET'])
+def getEmployee():
+	#error handling if no id is provided
+	if request.args.get('id') == None:
+		return redirect("/", code=302)
+	employee_id = request.args.get('id')
 	employee = employees.find_one({"_id": ObjectId(employee_id)})
 	organisation = organisations.find_one({"_id": ObjectId(employee['organisationID'])})
 	return render_template('employee.html', organisation=organisation, employee=employee)
 
+# inserts employee into the employees collection
+# based on info submitted in post request
+@app.route('/addEmployee',methods=['POST'])
+def addEmployee():
+	request_information = request.form
+	employees.insert({"name": request_information['name'], "contact": request_information['contact'],
+ 					  "organisationID": request_information['id']})
+	organisation_id = request_information['id']
+	organisation = organisations.find_one({"_id": ObjectId(organisation_id)})
+	employeesDump = employees.find({"organisationID":organisation_id})
+	return render_template('organisation.html', organisation=organisation,employees=employeesDump)
+
+# udpates the information relating to the employee
+# specified in the form data
+@app.route('/updateEmployee',methods=['POST'])
+def updateEmployee():
+	request_information = request.form
+	employees.update_one({'_id': ObjectId(request_information['id'])}, {
+		'$set': {"name": request_information['name'], "contact": request_information['contact']}
+	}, upsert=False)
+	employee_id = request_information['id']
+	employee = employees.find_one({"_id": ObjectId(employee_id)})
+	organisation = organisations.find_one({"_id": ObjectId(employee['organisationID'])})
+	return render_template('employee.html', organisation=organisation, employee=employee)
+
+
+# deletes the employee from the collection
 @app.route('/deleteEmployee',methods=['POST'])
 def deleteEmployee():
 	request_information = request.form
